63153d3513d611472d14c738a930a8cc340cfabc
actobotics <evolveproject@5x2.de>
2025-12-21 20:15:32 +0100

    Docs: Add loading screen with hero image preload

--- docs-site/.vitepress/theme/index.ts
+++ docs-site/.vitepress/theme/index.ts
@@ -1,6 +1,9 @@
 // Custom theme extending default VitePress theme
 import DefaultTheme from 'vitepress/theme'
+import Layout from './Layout.vue'
 import './custom.css'
 
-export default DefaultTheme
-
+export default {
+  extends: DefaultTheme,
+  Layout
+}
